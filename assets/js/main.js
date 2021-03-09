6a438c0747a78c55f8f9b906c375d8a369de2439
Aliasger290 <58413457+Aliasger290@users.noreply.github.com>
2021-03-09 13:37:11 +0530

    Edited some text and changed some images.
    Altruist Hospitality is a new Argo-trading company in Navi-Mumbai. This is a demo website for their business.

--- assets/js/main.js
+++ assets/js/main.js
@@ -104,6 +104,7 @@ sr.reveal(`.home__data, .home__img,
             .services__content, .menu__content,
             .app__data, .app__img,
             .contact__data, .contact__button,
-            .footer__content, .footer-main-title, .contact-us-img, .section-subtitle, .section-title`, {
+            .contact-us-img, .section-subtitle, 
+            .section-title`, {
     interval: 200
 })
